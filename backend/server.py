6925fa1b6ef2583205023049f1715927057cb337
oscarceran-art <oscar.ceran@outlook.com>
2026-08-05 13:23:53 +0000

    Fix CORS: remove allow_credentials=True incompatible with wildcard origins

--- backend/server.py
+++ backend/server.py
@@ -2987,8 +2987,8 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_credentials=True,
     allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
